a86a313de119c286ea5810c359cef7b8f954ec07
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-08-07 23:42:05 +0530

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
-echo -e "\u001b[32;1mSetting up refind-dark...\u001b[0m"
-echo -e "\u001b[31;1mWarning! Admin password is required.\u001b[0m"
+#!/bin/bash
+
+function setup_refind {
 sudo mkdir -p /boot/efi/EFI/refind/refind-dark
 echo "include refind-dark/refind-dark.conf" | sudo tee /boot/efi/EFI/refind/refind.conf
 sudo cp -r icons /boot/efi/EFI/refind/refind-dark/
 sudo cp -iv refind-dark.conf /boot/efi/EFI/refind/refind-dark/refind-dark.conf
+}
+
+echo -e "\u001b[32;1mSetting up refind-dark...\u001b[0m"
+echo -e "\u001b[31;1mWarning! sudo password is required.\u001b[0m"
+setup_refind
 echo -e "\u001b[34;1mDone.\u001b[0m"
